64bdf2eae10c100d4173b6b6461def86b394182a
Donny M <Mobslayer205@gmail.com>
2019-10-14 21:52:40 -0400

    Movement nearly complete: Fixed and finished sprite animations on player. Uses GetAxisRaw (vs GetAxis) for more rigid movements. TODO: Fix bug where player moves faster when two adjacent inputs are pressed (W+D is faster than just W)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float playerSpeed = 5;
 
     Animator animator;
 
@@ -14,13 +17,23 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         ProcessMovement();
     }
 
     private void ProcessMovement()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        transform.position = transform.position + movement *(playerSpeed / 10) * Time.deltaTime;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        if(Math.Abs(movement.magnitude) > 0)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
     }
 }
