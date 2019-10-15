db6bd2921e5e8fbcc43d21119d7ff27e9b08f1c4
Donny M <Mobslayer205@gmail.com>
2019-10-14 21:06:22 -0400

    Initial commit. Horizontal movement with animations.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        ProcessMovement();
+    }
+
+    private void ProcessMovement()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+    }
+}
