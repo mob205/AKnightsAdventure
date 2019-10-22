8658dededf1e2408ebe0213b2de39219f1f85bf9
Donny M <Mobslayer205@gmail.com>
2019-10-21 21:54:01 -0400

    Fixed movement glitch where player would move slightly faster if holding down two input buttons (ex: W and D together to move slightly faster to the north east.) Imported first basic tileset, now using tilemaps for map creation. Turned off anti-aliasing. Added a physics material with low friction so player does not experience friction when moving up against a collider. General asset folder organization.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,9 +28,9 @@ public class PlayerMovement : MonoBehaviour {
     private void ProcessMovement()
     {
         if (!canMove) { rb.velocity = Vector2.zero; return; }
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
-        //transform.position = transform.position + movement * playerSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f).normalized;
         rb.velocity = new Vector2(movement.x * playerSpeed, movement.y * playerSpeed);
+        Debug.Log(rb.velocity.magnitude);
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
