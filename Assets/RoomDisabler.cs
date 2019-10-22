37ae8c6a1def184bd69f4a73fc5ff9b242e77907
Donny M <Mobslayer205@gmail.com>
2019-10-22 16:58:11 -0400

    Created a game object that disables every room (except starting room) on game start.

---
+++ Assets/RoomDisabler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDisabler : MonoBehaviour
+{
+    public Level startingRoom;
+
+    void Start()
+    {
+        Level[] rooms = FindObjectsOfType<Level>();
+        foreach(Level room in rooms)
+        {
+            if(room != startingRoom)
+            {
+                room.gameObject.SetActive(false);
+            }
+        }
+    }
+
+   
+}
