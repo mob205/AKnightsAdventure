e51b3267959ca387f80ecdc3c6c4bcf1a9d18eb2
Donny M <Mobslayer205@gmail.com>
2019-12-15 20:29:35 -0500

    Successfully moved all of the "dark shadow" functionality to the parent enemy class. The dark shadow enemy type was standard in terms of how an enemy should function. Code reusability for different enemies with special behaviors (ex. fireball mage enemy?)

--- Assets/Scripts/Enemies/DarkShadow.cs
+++ Assets/Scripts/Enemies/DarkShadow.cs
@@ -4,170 +4,170 @@ using UnityEngine;
 public class DarkShadow : Enemy
 {
 
-    PlayerMovement player;
-    bool isAggro;
-    Vector3 defaultPos;
-    Animator animator;
-    Rigidbody2D rb;
+    //PlayerMovement player;
+    //bool isAggro;
+    //Vector3 defaultPos;
+    //Animator animator;
+    //Rigidbody2D rb;
 
-    public Vector3 movementDir;
-    bool canMove = true;
-    bool canAttack = true;
+    //public Vector3 movementDir;
+    //bool canMove = true;
+    //bool canAttack = true;
 
-    bool isKnockbacked;
-    float knockbackDuration;
-    bool isDead;
-    bool deathSeqStarted;
+    //bool isKnockbacked;
+    //float knockbackDuration;
+    //bool isDead;
+    //bool deathSeqStarted;
 
-    void Start()
-    {
-        animator = GetComponentInChildren<Animator>();
-        player = PlayerMovement.instance;
-        defaultPos = transform.position;
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //void Start()
+    //{
+    //    animator = GetComponentInChildren<Animator>();
+    //    player = PlayerMovement.instance;
+    //    defaultPos = transform.position;
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
 
-    void Update()
-    {
-        if (PlayerCombat.instance.isDead) { return; }
-        CheckAggro();
-        Move();
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            StartDeath();
-        }
-    }
-    void ToggleMove(bool moveability)
-    {
-        if (isKnockbacked) { return; }
-        canMove = moveability;
-    }
-    void CheckAggro()
-    {
-        if (Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
-        {
-            isAggro = true;
-        }
-        else
-        {
-            isAggro = false;
-        }
-    }
-    void Move()
-    {
-        if (isDead || !canMove) { return; }
-        if (isAggro)
-        {
-            if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
-            {
-                Attack();
-            }
-            else
-            {
-                //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
-                movementDir = (player.transform.position - transform.position).normalized;
-                rb.velocity = movementDir * movementSpeed;
+    //void Update()
+    //{
+    //    if (PlayerCombat.instance.isDead) { return; }
+    //    CheckAggro();
+    //    Move();
+    //    if (Input.GetKeyDown(KeyCode.Z))
+    //    {
+    //        StartDeath();
+    //    }
+    //}
+    //void ToggleMove(bool moveability)
+    //{
+    //    if (isKnockbacked) { return; }
+    //    canMove = moveability;
+    //}
+    //void CheckAggro()
+    //{
+    //    if (Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+    //    {
+    //        isAggro = true;
+    //    }
+    //    else
+    //    {
+    //        isAggro = false;
+    //    }
+    //}
+    //void Move()
+    //{
+    //    if (isDead || !canMove) { return; }
+    //    if (isAggro)
+    //    {
+    //        if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+    //        {
+    //            Attack();
+    //        }
+    //        else
+    //        {
+    //            //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+    //            movementDir = (player.transform.position - transform.position).normalized;
+    //            rb.velocity = movementDir * movementSpeed;
 
-                animator.SetBool("IsMoving", true);
-            }
-        } else if (!isAggro)
-        {
-            if (Vector3.Distance(transform.position, defaultPos) > 1f)
-            {
-                //transform.position = Vector3.MoveTowards(transform.position, defaultPos, movementSpeed * Time.deltaTime);
-                movementDir = (defaultPos - transform.position).normalized;
-                animator.SetBool("IsMoving", true);
-                rb.velocity = movementDir * movementSpeed;
-            }
-            else
-            {
-                animator.SetBool("IsMoving", false);
-                rb.velocity = Vector3.zero;
-            }
-        }
+    //            animator.SetBool("IsMoving", true);
+    //        }
+    //    } else if (!isAggro)
+    //    {
+    //        if (Vector3.Distance(transform.position, defaultPos) > 1f)
+    //        {
+    //            //transform.position = Vector3.MoveTowards(transform.position, defaultPos, movementSpeed * Time.deltaTime);
+    //            movementDir = (defaultPos - transform.position).normalized;
+    //            animator.SetBool("IsMoving", true);
+    //            rb.velocity = movementDir * movementSpeed;
+    //        }
+    //        else
+    //        {
+    //            animator.SetBool("IsMoving", false);
+    //            rb.velocity = Vector3.zero;
+    //        }
+    //    }
 
-        animator.SetBool("IsAggro", isAggro);
-        animator.SetFloat("Horizontal", movementDir.x);
-        animator.SetFloat("Vertical", movementDir.y);
-    }
-    void StartDeath()
-    {
-        rb.velocity = Vector2.zero;
-        //animator.SetBool("IsDead", true);
-        animator.SetTrigger("OnDeath");
-    }
-    void Attack()
-    {
-        if (isKnockbacked || !canAttack) { return; }
-        canMove = false;
-        rb.velocity = Vector2.zero;
-        animator.SetTrigger("Attack");
-        canAttack = false;
-        StartCoroutine(AllowAttack());
-    }
-    IEnumerator AllowAttack()
-    {
-        yield return new WaitForSeconds(attackCD);
-        canAttack = true;
-    }
-    void StopAnimation()
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") || animator.GetCurrentAnimatorStateInfo(0).IsName("Damaged"))
-        {
-            animator.SetTrigger("StopAnimation");
-            canMove = true;
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
-        {
-            Debug.Log("Enemy has died.");
-            Destroy(gameObject);
-        }
-    }
-    void Knockback(Vector2 knockback, float duration)
-    {
-        if (isKnockbacked) {  return; }
+    //    animator.SetBool("IsAggro", isAggro);
+    //    animator.SetFloat("Horizontal", movementDir.x);
+    //    animator.SetFloat("Vertical", movementDir.y);
+    //}
+    //void StartDeath()
+    //{
+    //    rb.velocity = Vector2.zero;
+    //    //animator.SetBool("IsDead", true);
+    //    animator.SetTrigger("OnDeath");
+    //}
+    //void Attack()
+    //{
+    //    if (isKnockbacked || !canAttack) { return; }
+    //    canMove = false;
+    //    rb.velocity = Vector2.zero;
+    //    animator.SetTrigger("Attack");
+    //    canAttack = false;
+    //    StartCoroutine(AllowAttack());
+    //}
+    //IEnumerator AllowAttack()
+    //{
+    //    yield return new WaitForSeconds(attackCD);
+    //    canAttack = true;
+    //}
+    //void StopAnimation()
+    //{
+    //    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") || animator.GetCurrentAnimatorStateInfo(0).IsName("Damaged"))
+    //    {
+    //        animator.SetTrigger("StopAnimation");
+    //        canMove = true;
+    //    }
+    //    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+    //    {
+    //        Debug.Log("Enemy has died.");
+    //        Destroy(gameObject);
+    //    }
+    //}
+    //void Knockback(Vector2 knockback, float duration)
+    //{
+    //    if (isKnockbacked) {  return; }
 
-        knockbackDuration = duration;
-        StartCoroutine("ApplyKnockback", knockback);
-    }
-    IEnumerator ApplyKnockback(Vector2 knockback)
-    {
-        ToggleMove(false);
-        isKnockbacked = true;
+    //    knockbackDuration = duration;
+    //    StartCoroutine("ApplyKnockback", knockback);
+    //}
+    //IEnumerator ApplyKnockback(Vector2 knockback)
+    //{
+    //    ToggleMove(false);
+    //    isKnockbacked = true;
 
-        rb.velocity = Vector2.zero;
-        rb.AddForce(knockback, ForceMode2D.Impulse);
+    //    rb.velocity = Vector2.zero;
+    //    rb.AddForce(knockback, ForceMode2D.Impulse);
 
-        animator.SetBool("IsDamaged", true);
+    //    animator.SetBool("IsDamaged", true);
 
-        yield return new WaitForSeconds(knockbackDuration);
+    //    yield return new WaitForSeconds(knockbackDuration);
 
-        animator.SetBool("IsDamaged", false);
+    //    animator.SetBool("IsDamaged", false);
 
-        isKnockbacked = false;
-        ToggleMove(true);
+    //    isKnockbacked = false;
+    //    ToggleMove(true);
 
-        if (isDead)
-        {
-            isKnockbacked = true;
-            StartDeath();
-        }
-    }
+    //    if (isDead)
+    //    {
+    //        isKnockbacked = true;
+    //        StartDeath();
+    //    }
+    //}
 
-    void Damage(int amount)
-    {
-        health -= amount;
-        if (health <= 0)
-        {
-            isDead = true;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("PlayerAttack"))
-        {
-            Damage(PlayerCombat.instance.damage);
-            Knockback((transform.position - player.transform.position).normalized * PlayerCombat.instance.knockback, 1f);
-        }
-    }
+    //void Damage(int amount)
+    //{
+    //    health -= amount;
+    //    if (health <= 0)
+    //    {
+    //        isDead = true;
+    //    }
+    //}
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("PlayerAttack"))
+    //    {
+    //        Damage(PlayerCombat.instance.damage);
+    //        Knockback((transform.position - player.transform.position).normalized * PlayerCombat.instance.knockback, 1f);
+    //    }
+    //}
 }
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -18,13 +18,172 @@ public class Enemy : MonoBehaviour
     public int attackRange;
 
 
+    protected bool isAggro;
+    protected bool canMove = true;
+    protected bool canAttack = true;
+    protected bool isKnockbacked;
+    protected bool isDead;
 
-    void Start()
+    protected PlayerMovement player;
+    protected Vector3 defaultPos;
+    protected Rigidbody2D rb;
+    protected Animator animator;
+
+    protected Vector3 movementDir;
+    protected float knockbackDuration;
+
+    protected void Start()
     {
+        animator = GetComponentInChildren<Animator>();
+        player = PlayerMovement.instance;
+        defaultPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    protected void Update()
     {
-        
+        if (PlayerCombat.instance.isDead) { return; }
+        CheckAggro();
+        Move();
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartDeath();
+        }
     }
+    protected void ToggleMove(bool moveability)
+    {
+        if (isKnockbacked) { return; }
+        canMove = moveability;
+    }
+    protected void CheckAggro()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+        {
+            isAggro = true;
+        }
+        else
+        {
+            isAggro = false;
+        }
+    }
+    protected void Move()
+    {
+        if (isDead || !canMove) { return; }
+        if (isAggro)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+            {
+                Attack();
+            }
+            else
+            {
+                //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+                movementDir = (player.transform.position - transform.position).normalized;
+                rb.velocity = movementDir * movementSpeed;
+
+                animator.SetBool("IsMoving", true);
+            }
+        }
+        else if (!isAggro)
+        {
+            if (Vector3.Distance(transform.position, defaultPos) > 1f)
+            {
+                //transform.position = Vector3.MoveTowards(transform.position, defaultPos, movementSpeed * Time.deltaTime);
+                movementDir = (defaultPos - transform.position).normalized;
+                animator.SetBool("IsMoving", true);
+                rb.velocity = movementDir * movementSpeed;
+            }
+            else
+            {
+                animator.SetBool("IsMoving", false);
+                rb.velocity = Vector3.zero;
+            }
+        }
+
+        animator.SetBool("IsAggro", isAggro);
+        animator.SetFloat("Horizontal", movementDir.x);
+        animator.SetFloat("Vertical", movementDir.y);
+    }
+    protected void StartDeath()
+    {
+        rb.velocity = Vector2.zero;
+        //animator.SetBool("IsDead", true);
+        animator.SetTrigger("OnDeath");
+    }
+    protected void Attack()
+    {
+        if (isKnockbacked || !canAttack) { return; }
+        canMove = false;
+        rb.velocity = Vector2.zero;
+        animator.SetTrigger("Attack");
+        canAttack = false;
+        StartCoroutine(AllowAttack());
+    }
+    protected IEnumerator AllowAttack()
+    {
+        yield return new WaitForSeconds(attackCD);
+        canAttack = true;
+    }
+    protected void StopAnimation()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") || animator.GetCurrentAnimatorStateInfo(0).IsName("Damaged"))
+        {
+            animator.SetTrigger("StopAnimation");
+            canMove = true;
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+        {
+            Debug.Log("Enemy has died.");
+            Destroy(gameObject);
+        }
+    }
+    protected void Knockback(Vector2 knockback, float duration)
+    {
+        if (isKnockbacked) { return; }
+
+        knockbackDuration = duration;
+        StartCoroutine("ApplyKnockback", knockback);
+    }
+    protected IEnumerator ApplyKnockback(Vector2 knockback)
+    {
+        ToggleMove(false);
+        isKnockbacked = true;
+
+        rb.velocity = Vector2.zero;
+        rb.AddForce(knockback, ForceMode2D.Impulse);
+
+        animator.SetBool("IsDamaged", true);
+
+        yield return new WaitForSeconds(knockbackDuration);
+
+        animator.SetBool("IsDamaged", false);
+
+        isKnockbacked = false;
+        ToggleMove(true);
+
+        if (isDead)
+        {
+            isKnockbacked = true;
+            StartDeath();
+        }
+    }
+
+    protected void Damage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            isDead = true;
+        }
+    }
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerAttack"))
+        {
+            Damage(PlayerCombat.instance.damage);
+            Knockback((transform.position - player.transform.position).normalized * PlayerCombat.instance.knockback, 1f);
+        }
+    }
+
+
 }
